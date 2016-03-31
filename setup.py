4b346f7899e881e64395a94ddbbeb25a869a2a3b
Phuong <boingonline@gmail.com>
2016-03-31 16:17:21 +0700

    Test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-emojione',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     py_modules=['django_emojione'],
     include_package_data=True,
@@ -18,7 +18,6 @@ setup(
     author_email='boingonline@gmail.com',
     classifiers=[
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
