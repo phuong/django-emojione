c541e780e038e823b12dcad2a21e6d3b18ff3e2e
Phuong Vu <boingonline@gmail.com>
2017-04-12 10:01:36 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-emojione',
-    version='0.1.7.2',
+    version='0.1.7.3',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
